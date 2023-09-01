2a2c8398431ce873aff4015b2b7a6c9c02236a6e
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-09-01 22:01:01 +0800

    Added story intro animation and scene div

--- src/views/StoryPlaybackView.vue
+++ src/views/StoryPlaybackView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // Scripts for the component
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { StoryInfo, getStoryInfo } from '../scripts/story';
 
 const props = defineProps({
@@ -12,30 +12,74 @@ const storyInfo = ref<StoryInfo>(
     decodeURIComponent(props.storyInfoDir as string)
   )
 );
+
+const isPlaying = ref(false);
+
+function playbackIntroAnimation() {
+  const splash = document.getElementById('splash') as HTMLElement;
+  const scene = document.getElementById('scene') as HTMLElement;
+
+  // Set transition timings for both elements.
+  splash.style.transition = 'opacity 1s ease-in-out';
+  scene.style.transition = 'opacity 0.5s ease-in-out';
+
+  // Set opacity to 1 for splash screen.
+  splash.style.opacity = '1';
+
+  // Wait for three seconds before setting splash screen's opacity to zero again.
+  const timeout1 = 3000;
+  setTimeout(() => {
+    splash.style.opacity = '0';
+  }, timeout1);
+
+  // While no elements are visible, toggle visibility boolean.
+  const timeout2 = 1000;
+  setTimeout(() => {
+    isPlaying.value = true;
+  }, timeout1 + timeout2);
+
+  // Wait for half a second before setting scene's opacity to one.
+  const timeout3 = 500;
+  setTimeout(() => {
+    scene.style.opacity = '1';
+  }, timeout1 + timeout2 + timeout3);
+}
+
+onMounted(() => {
+  playbackIntroAnimation();
+})
 </script>
 
 <template>
   <div class="container">
     <!-- HTML elements for the component -->
-    <div class="splash" @click="$router.go(-1)">
+    <div id="splash" :hidden="isPlaying" @click="$router.go(-1)">
       <h1>{{ storyInfo.title }}</h1>
+    </div>
+    <div id="scene" :hidden="!isPlaying" @click="$router.go(-1)">
+      <h1>Scene</h1>
     </div>
   </div>
 </template>
 
 <style scoped>
 /* CSS styles for the component */
-.splash {
+#splash {
   position: absolute;
   width: 100vw;
   height: 100vh;
-  /* background-color: #000; */
+  opacity: 0;
 }
 
-.splash h1 {
+#splash h1 {
   font-size: 45px;
   position: relative;
   line-height: 100vh;
   text-align: center;
+}
+
+#scene {
+  margin: 30px;
+  opacity: 0;
 }
 </style>
