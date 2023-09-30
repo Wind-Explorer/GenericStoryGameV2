483fb46eae44024106d6566523e603391108927c
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-09-30 19:21:55 +0800

    Added rust invoke in TS

--- src/scripts/rustBackend.ts
+++ src/scripts/rustBackend.ts
@@ -1,9 +1,17 @@
 import { invoke } from "@tauri-apps/api";
 
-export class RustBackend {
-  static compressFolder(sourceDir: string, destinationDir: string, outputName: string) {
-    return invoke("compress_folder", { folderPath: sourceDir, destinationPath: destinationDir, outputName });
+export class ArchiveManager {
+  static compressFolder(folderPath: string, outputPath: string) {
+    return invoke("compress_folder", { folderPath: folderPath, outputPath: outputPath });
   }
+
+  static decompressArchive(archivePath: string, outputFolder: string) {
+    return invoke("decompress_archive", { archivePath: archivePath, outputFolder: outputFolder });
+  }
+}
+
+export class RustBackend {
+  static archiveManager = ArchiveManager;
 
   static async resolveCurrentOS(): Promise<string> {
     return await invoke('resolve_current_os');
