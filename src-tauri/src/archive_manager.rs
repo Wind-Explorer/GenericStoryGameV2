1a54b70bc2ee666e3666b47348510d55eba821b5
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-09-30 18:43:56 +0800

    Refined archive management in Rust

---
+++ src-tauri/src/archive_manager.rs
@@ -0,0 +1,96 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use zip::write::FileOptions;
+use zip::{ZipArchive, ZipWriter};
+
+/// Adds a folder to a zip file
+/// # Arguments
+/// * `zip` - The zip file to add the folder to
+/// * `root_path` - The root path of the folder to add
+/// * `folder_path` - The path of the folder to add
+/// # Returns
+/// * `std::io::Result<()>` - The result of the operation
+fn add_folder_to_zip(
+    zip: &mut ZipWriter<File>,
+    folder_path: &Path,
+    base_path: &Path,
+) -> zip::result::ZipResult<()> {
+    for entry in folder_path.read_dir()? {
+        let path = entry?.path();
+        if path.is_dir() {
+            add_folder_to_zip(zip, &path, base_path)?;
+        } else {
+            let relative_path = path.strip_prefix(base_path).unwrap();
+            let options = FileOptions::default().unix_permissions(0o755);
+            let mut file = File::open(&path)?;
+            let mut buffer = Vec::new();
+            file.read_to_end(&mut buffer)?;
+            zip.start_file(relative_path.to_str().unwrap(), options)?;
+            zip.write_all(&buffer)?;
+        }
+    }
+    Ok(())
+}
+
+/// Compresses a folder into a zip file
+/// # Arguments
+/// * `source_folder_path` - The path to the folder to compress
+/// * `output_path` - The path to the output zip file
+/// # Returns
+/// * `zip::result::ZipResult<()>` - The result of the operation
+/// # Example
+/// ```rs
+/// let folder_path = "/Users/wind/Desktop";
+/// let output_path = "/Users/wind/test.zip";
+/// match compress_folder(folder_path, output_path) {
+///     Ok(_) => println!("Success"),
+///     Err(e) => println!("Error: {:?}", e),
+/// }
+/// ```
+pub fn compress_folder(source_folder_path: &str, output_path: &str) -> zip::result::ZipResult<()> {
+    let folder_path = Path::new(source_folder_path);
+    let output_file = File::create(output_path)?;
+    let mut zip = ZipWriter::new(output_file);
+
+    add_folder_to_zip(&mut zip, folder_path, folder_path)?;
+
+    zip.finish()?;
+    Ok(())
+}
+
+/// Decompresses a zip file into a folder
+/// # Arguments
+/// * `archive_path` - The path to the zip file to decompress
+/// * `output_folder` - The path to the output folder
+/// # Returns
+/// * `zip::result::ZipResult<()>` - The result of the operation
+/// # Example
+/// ```rs
+/// let archive_path = "/Users/wind/test.zip";
+/// let output_folder = "/Users/wind/test";
+/// match decompress_archive(archive_path, output_folder) {
+///     Ok(_) => println!("Success"),
+///     Err(e) => println!("Error: {:?}", e),
+/// }
+/// ```
+pub fn decompress_archive(archive_path: &str, output_folder: &str) -> zip::result::ZipResult<()> {
+    let archive_file = File::open(archive_path)?;
+    let mut archive = ZipArchive::new(archive_file)?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let output_path = Path::new(output_folder).join(file.name());
+        if file.is_dir() {
+            std::fs::create_dir_all(&output_path)?;
+        } else {
+            if let Some(parent) = output_path.parent() {
+                std::fs::create_dir_all(parent)?;
+            }
+            let mut output_file = File::create(&output_path)?;
+            std::io::copy(&mut file, &mut output_file)?;
+        }
+    }
+
+    Ok(())
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{
-    fs::File,
-    io::{Read, Write},
-    path::{Path, PathBuf},
-};
-
-use zip::{write::FileOptions, ZipWriter};
+mod archive_manager;
 
 #[tauri::command]
 fn resolve_current_os() -> String {
@@ -19,63 +13,28 @@ fn resolve_current_os() -> String {
     return "xdg".to_string();
 }
 
-/// Compresses a folder into a zip file
-/// # Arguments
-/// * `dir` - The directory to compress
-/// * `destination` - The destination directory
-/// * `file_name` - The name of the zip file
 #[tauri::command]
-fn compress_folder(source_dir: String, destination_dir: String, output_name: String) {
-    // Path to the folder to compress
-    let source_dir_path = Path::new(source_dir.as_str());
-
-    // Path to the output zip file
-    let output_file_path = File::create(
-        PathBuf::from(destination_dir)
-            .join(PathBuf::from(output_name)
-                .with_extension("zip")
-    )).unwrap();
-
-    // Create the zip file
-    let mut zip = ZipWriter::new(output_file_path);
-    add_folder_to_zip(&mut zip, &source_dir_path, &source_dir_path).unwrap();
+fn compress_folder(folder_path: String, output_path: String) -> bool {
+    match archive_manager::compress_folder(folder_path.as_str(), output_path.as_str()) {
+        Ok(_) => return true,
+        Err(_) => return false,
+    };
 }
 
-/// Adds a folder to a zip file
-/// # Arguments
-/// * `zip` - The zip file to add the folder to
-/// * `root_path` - The root path of the folder to add
-/// * `folder_path` - The path of the folder to add
-/// # Returns
-/// * `std::io::Result<()>` - The result of the operation
-fn add_folder_to_zip(
-    zip: &mut ZipWriter<File>,
-    root_path: &Path,
-    folder_path: &Path,
-) -> std::io::Result<()> {
-    for entry in folder_path.read_dir()? {
-        let path = entry?.path();
-        if path.is_dir() {
-            add_folder_to_zip(zip, root_path, &path)?;
-        } else {
-            let relative_path = path.strip_prefix(root_path).unwrap();
-            let options =
-                FileOptions::default().compression_method(zip::CompressionMethod::Deflated);
-            zip.start_file(relative_path.to_str().unwrap(), options)?;
-            let mut file = File::open(&path)?;
-            let mut buffer = Vec::new();
-            file.read_to_end(&mut buffer)?;
-            zip.write_all(&buffer)?;
-        }
-    }
-    Ok(())
+#[tauri::command]
+fn decompress_archive(archive_path: String, output_folder: String) -> bool {
+    match archive_manager::decompress_archive(archive_path.as_str(), output_folder.as_str()) {
+        Ok(_) => return true,
+        Err(_) => return false,
+    };
 }
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             resolve_current_os,
-            compress_folder
+            compress_folder,
+            decompress_archive
         ])
         .on_window_event(|e| {
             if let tauri::WindowEvent::Resized(_) = e.event() {
