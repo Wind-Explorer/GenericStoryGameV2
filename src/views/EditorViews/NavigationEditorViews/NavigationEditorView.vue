9f8900734a82900b9896fe3e243638945e94cced
ZhichGaming <nick.zhicheng@gmail.com>
2023-12-25 21:29:30 -0500

    Feat: Drag and drop nodes on canvas
    I also removed drag prevention on all elements in css to only apply to images, since that prevents drag&dropping nodes from working.

--- src/views/EditorViews/NavigationEditorViews/NavigationEditorView.vue
+++ src/views/EditorViews/NavigationEditorViews/NavigationEditorView.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 // Scripts for the component
 import { House, InfoFilled } from '@element-plus/icons-vue';
-import { VueFlow, XYPosition, isNode, Position, CoordinateExtent, Elements, ConnectionMode } from '@vue-flow/core'
-import { ref } from 'vue'
+import { VueFlow, XYPosition, isNode, Position, Elements, ConnectionMode, useVueFlow } from '@vue-flow/core'
+import { nextTick, ref, watch } from 'vue'
 // import { ScenesManager } from '../../../scripts/scenesManager';
-import { SceneInfo, resolveSceneInfo, resolveStoryInfo } from '../../../scripts/story';
+import { ExtraSceneInfo, SceneInfo, resolveSceneInfo, resolveScenesFromFS, resolveStoryInfo } from '../../../scripts/story';
 import { newUUID } from '../../../scripts/utils';
 import dagre from 'dagre'
 import { Controls } from '@vue-flow/controls'
 import { Background } from '@vue-flow/background'
 import '@vue-flow/controls/dist/style.css'
 import router from '../../../router';
+import { ScenesManager } from '../../../scripts/scenesManager';
 
 const props = defineProps({
   baseDir: String,
@@ -20,13 +21,22 @@ const baseDir = decodeURIComponent(props.baseDir as string);
 
 // var elements: Ref<Elements> = ref([])
 
-// const scenesManager = ref(new ScenesManager(await resolveScenesFromFS(baseDir), baseDir));
+const scenesManager = ref(new ScenesManager(await resolveScenesFromFS(baseDir), baseDir));
+
+const { findNode, onConnect, addEdges, addNodes, project, vueFlowRef } = useVueFlow()
 
 const dagreGraph = new dagre.graphlib.Graph()
 
 dagreGraph.setDefaultEdgeLabel(() => ({}))
 
 const elements = ref<Elements>([])
+
+elements.value.push({
+  id: "origin",
+  type: "input",
+  position: { x: 0, y: 0 },
+  label: "Origin",
+})
 
 function onLayout(direction: string) {
   const isHorizontal = direction === 'LR'
@@ -131,6 +141,66 @@ function pushEdge(sourceId: string, targetId: string) {
   })
 }
 
+let draggedScene: ExtraSceneInfo | null = null;
+
+function onDragStart(event: any, scene: any) {
+  if (event.dataTransfer) {
+    draggedScene = scene;
+    event.dataTransfer.effectAllowed = 'move'
+  }
+}
+
+function onDrop(event: any) {
+  // const scene = JSON.parse(event.dataTransfer?.getData('scene'))
+  const scene = draggedScene!.base_scene_info;
+
+  const { left, top, height } = vueFlowRef.value!.getBoundingClientRect()
+  // const {  } = vueFlowRef.value!.getBoundingClientRect()
+
+  // I don't know why, but the y-position is inverted. Left and top seem to be always 0.
+  const position = project({
+    x: event.clientX - left,
+    y: (top + height) - event.clientY - top,
+  })
+
+  const hasChildren = (scene.scene_actions.single_choice != null && scene.scene_actions.single_choice != "#END") || (scene.scene_actions.multiple_choice?.length ?? 0) > 0;
+  const label = scene.center_text ?? scene.narration_text ?? "No text";
+
+  const newNode = {
+    id: newUUID().toString(),
+    type: draggedScene!.scene_path == storyInfo.entry_point ? "input" : hasChildren ? "default" : "output",
+    position,
+    label: label.substring(0, 50) + (label.length > 50 ? "..." : ""),
+  }
+
+  // addNodes([newNode])
+
+  elements.value.push(newNode)
+
+  // align node position after drop, so it's centered to the mouse
+  // nextTick(() => {
+  //   const node = findNode(newNode.id)!
+  //   const stop = watch(
+  //     () => node.dimensions,
+  //     (dimensions) => {
+  //       if (dimensions.width > 0 && dimensions.height > 0) {
+  //         node.position = { x: node.position.x - node.dimensions.width / 2, y: node.position.y - node.dimensions.height / 2 }
+  //         stop()
+  //       }
+  //     },
+  //     { deep: true, flush: 'post' },
+  //   )
+  // })
+}
+
+function onDragOver(event: any) {
+  event.preventDefault()
+
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = 'move'
+  }
+}
+
 const storyInfo = await resolveStoryInfo(baseDir);
 const entryPoint = await resolveSceneInfo(storyInfo.entry_point);
 
@@ -146,10 +216,26 @@ onLayout('TB')
       <h1>Story navigation</h1>
       <el-button @click="$router.go(-1)" size="large" :icon="House" type="info" plain></el-button>
     </div>
-    <VueFlow v-model="elements" :connection-mode="ConnectionMode.Loose" @pane-ready="onLayout('TB')">
-      <Background/>
-      <Controls />
-    </VueFlow>
+    <div class="content" @dragend="onDrop($event)">
+      <VueFlow v-model="elements" :connection-mode="ConnectionMode.Loose" @pane-ready="onLayout('TB')" @dragover="onDragOver">
+        <Background/>
+        <Controls />
+      </VueFlow>
+      <hr style="height: 100%; border: none; border-left: 1px solid #77777777;">
+      <div class="events">
+        <h2>Events</h2>
+        <p>Drag and drop on the canvas.</p>
+        <hr style="margin-top: 5px; margin-bottom: 5px;">
+        <div class="event-list" v-for="scene in scenesManager.scenesList" :key="scene.scene_path">
+          <el-tag class="event-list-node" draggable="true" @dragstart="onDragStart($event, scene)">
+            {{ 
+              (scene.base_scene_info.center_text ?? scene.base_scene_info.narration_text ?? "").substring(0, 50) + 
+              ((scene.base_scene_info.center_text ?? scene.base_scene_info.narration_text ?? "").length > 50 ? "..." : "") 
+            }}
+          </el-tag>
+        </div>
+      </div>
+    </div>
     <div class="bottom-text">
       <el-icon>
         <InfoFilled />
@@ -188,5 +274,22 @@ onLayout('TB')
   border-top: 1px solid #77777777;
   padding-top: 8px;
   margin-top: 0;
+}
+
+.content {
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.events {
+  width: 350px;
+  padding: 10px;
+}
+
+.event-list-node {
+  margin-bottom: 5px;
 }
 </style>
